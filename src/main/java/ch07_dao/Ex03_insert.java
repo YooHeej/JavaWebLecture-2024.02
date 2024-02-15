2ab485979e4f577c4bb234b5f28b5802b6c81476
YooHeej <sprite1139@gmail.com>
2024-02-15 13:13:35 +0900

    02151313

---
+++ src/main/java/ch07_dao/Ex03_insert.java
@@ -0,0 +1,27 @@
+package ch07_dao;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/ch07/city/list")
+public class Ex03_insert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    // 입력 폼 보여주기
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/ch07/list.jsp");
+		rd.forward(request, response);
+	}
+
+	// 사용자 입력 처리하기
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
