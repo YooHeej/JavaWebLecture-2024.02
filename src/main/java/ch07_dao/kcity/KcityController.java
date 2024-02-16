12fe66b3079a5194ec613e6a922aa866dc24bc60
YooHeej <sprite1139@gmail.com>
2024-02-16 17:25:24 +0900

    02161725

--- src/main/java/ch07_dao/kcity/KcityController.java
+++ src/main/java/ch07_dao/kcity/KcityController.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import ch07_dao.City;
